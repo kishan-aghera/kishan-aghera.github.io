bfe714c5057049646864f665d0c76215f3bfda6b
Kishan Aghera <55836816+kishan-aghera@users.noreply.github.com>
2021-07-12 11:19:20 +0530

    Added NavBar with react-burger-menu

--- src/components/App.js
+++ src/components/App.js
@@ -8,31 +8,38 @@ import Contact from './Contact/Contact';
 import Home from './Home/Home';
 import Projects from './Projects/Projects';
 import Skills from './Skills/Skills';
+import NavBar from './NavBar/NavBar';
 
 
 const App = () => {
   return (
     <Fragment>
       <BrowserRouter>
+        <NavBar />
         <Route
           path="/"
           component={Home}
+          exact
         />
         <Route
           path="/about"
           component={About}
+          exact
         />
         <Route
           path="/projects"
           component={Projects}
+          exact
         />
         <Route
           path="/skills"
           component={Skills}
+          exact
         />
         <Route
           path="/contact"
           component={Contact}
+          exact
         />
       </BrowserRouter>
     </Fragment>
