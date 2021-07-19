de4fb9326b354ee1cbaadf36e0ad36e583b46e7a
Kishan Aghera <55836816+kishan-aghera@users.noreply.github.com>
2021-07-19 11:03:04 +0530

    Updated Contact component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,86 @@
 import React from 'react';
 
+import './Contact.css';
+
+import Header from '../Header/Header';
+import FooterLink from '../FooterLink/FooterLink';
+
+import contactVector from '../../assets/contact_anime.png';
+import github from '../../assets/gh.png';
+import linkedin from '../../assets/li.png';
+
 const Contact = () => {
 	return (
 		<div className="section-container">
-			In Contact Page
+			<Header
+				heading="Get in touch."
+				details="Interested to collaborate? Feel free to drop me an email."
+			/>
+
+			<div className="contact-form-container">
+				<form
+					action="https://formspree.io/f/mayadprl"
+					method="POST"
+					className="contact-form"
+				>
+					<input
+						type="email"
+						placeholder="Email"
+						name="_replyto"
+						className="input-box email-input"
+					/>
+
+					<textarea
+						type="text"
+						placeholder="Your Message"
+						name="message"
+						className="input-box body-input"
+					/>
+
+					<button type="submit" className="contact-btn">
+						Send Email
+					</button>
+				</form>
+			</div>
+
+			<div className="social-icons-container">
+				<a
+					target="_blank"
+					rel="noopener noreferrer"
+					href="https://github.com/kishan-aghera"
+					className="social-icon"
+				>
+					<img
+						src={github}
+						alt="Github logo"
+					/>
+				</a>
+
+				<a
+					target="_blank"
+					rel="noopener noreferrer"
+					href="https://linkedin.com/in/kishan-aghera"
+					className="social-icon"
+				>
+					<img
+						src={linkedin}
+						alt="Linkedin logo"
+					/>
+				</a>
+			</div>
+
+			<FooterLink
+				phrase="Read more "
+				link="about me!"
+				toAddress="/about"
+			/>
+
+			<div className="vector-frame">
+				<img
+					src={contactVector}
+					alt="Contact Vector"
+				/>
+			</div>
 		</div>
 	)
 }
